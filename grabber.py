d4a5d53d1c9a584d46287de90ad608d67b02c0c7
Dima <4ainsmoker@gmail.com>
2021-08-18 00:19:35 +0300

    tst

--- grabber.py
+++ grabber.py
@@ -1,7 +1,6 @@
 import requests
 import json
 import pickle
-import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from wordcloud import WordCloud
@@ -19,11 +18,21 @@ def loadData(filename: str):
         return data
 
 
+def textFilter(text: str):
+    #Cписок для отсчения конструкция can, when и т.д.
+    stop_words = set(stopwords.words('english'))
+    # Производим токенизацию
+    word_tokens = word_tokenize(text)
+    return [word for word in word_tokens if word not in stop_words and word.isalpha() and word != 'chars']
+
+
 def grabber(url):
-    # api =
-    # r = requests.get(url+f'&apiKey={api}')
-    # saveData(r,'request')
-    r = loadData('request.pickle')
+    if False:
+        api_key = 0  # ваш api key
+        r = requests.get(url + f'&apiKey={api_key}')
+        saveData(r, 'request')
+    else:
+        r = loadData('request.pickle')
     json_text = json.loads(r.text)
     # print(js)
     text = ""
@@ -31,26 +40,22 @@ def grabber(url):
     for obj in json_text['articles']:
         text += str(obj['content']).lower()
 
-    stop_words = set(stopwords.words('english'))
-    word_tokens = word_tokenize(text)
-    filtered_text = [word for word in word_tokens if word not in stop_words and word.isalpha() and word != 'chars']
+    filtered_text = textFilter(text)
 
     data_frequencies = {}
 
     for word in filtered_text:
         data_frequencies[word] = data_frequencies.get(word, 0) + 1
 
-    dataFrequencies50 = dict(sorted(data_frequencies.items(), key=lambda f: -f[1])[:50])
-    print(dataFrequencies50)
-    word_cloud = WordCloud(width=900, height=500, max_words=50, background_color="white", relative_scaling=1,
-                           normalize_plurals=False).generate_from_frequencies(dataFrequencies50)
-
-    plt.imshow(word_cloud, interpolation='bilinear')
+    result = dict(sorted(data_frequencies.items(), key=lambda i: i[1], reverse=True)[:50])
+    print(result)
+    word_cloud = WordCloud(width=1920, height=1080, max_words=50, background_color="white", relative_scaling=1,
+                           normalize_plurals=False).generate_from_frequencies(result)
+    plt.imshow(word_cloud)
     plt.axis("off")
     plt.show()
 
 
-# response = requests.get('https://newsapi.org/v2/everything?q=russia&pageSize=100&page=10&from=2019-01-23&to=2019-01-23&apiKey=KEY')
-
 if __name__ == '__main__':
-    grabber('https://newsapi.org/v2/everything?q=russia&from=2021-08-01')
+    params = 'country=us&language=en&q=russia&from=2021-08-01'
+    grabber(f'https://newsapi.org/v2/everything?{params}')
